d1ef1d747b711fd32e54a36909cc531874404fbe
Pamyc <163673087+Pamyc@users.noreply.github.com>
2025-09-30 11:12:35 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '50px' }}>
+      <h1>Hello, Timeweb!</h1>
+      <p>Это тестовый Next.js проект для проверки деплоя 🚀</p>
+    </div>
+  );
+}
